631a002b3b891ec069a951eb7265e64fd34a3dde
Cierra_Runis <2864283875@qq.com>
2023-10-18 17:27:48 +0800

    🌞Init Repository🌞

---
+++ src/main/kotlin/pers/cierra_runis/mine_net/Mine_NetDataGenerator.kt
@@ -0,0 +1,9 @@
+package pers.cierra_runis.mine_net
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object Mine_NetDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
